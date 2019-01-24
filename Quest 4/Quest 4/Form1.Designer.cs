c4fb7ea40bc33c0f50e506d20e621cb2729f14d6
scott vereen <scottvereen@users.noreply.github.com>
2019-01-24 11:21:17 -0500

    Upload

---
+++ Quest 4/Quest 4/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Quest_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.dtpdate = new System.Windows.Forms.DateTimePicker();
+            this.cmbState = new System.Windows.Forms.ComboBox();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.ldl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtHome = new System.Windows.Forms.TextBox();
+            this.txtCell = new System.Windows.Forms.TextBox();
+            this.txtWork = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.txtWork);
+            this.groupBox1.Controls.Add(this.txtCell);
+            this.groupBox1.Controls.Add(this.txtHome);
+            this.groupBox1.Controls.Add(this.txtZip);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.dtpdate);
+            this.groupBox1.Controls.Add(this.cmbState);
+            this.groupBox1.Controls.Add(this.lbl10);
+            this.groupBox1.Controls.Add(this.lbl9);
+            this.groupBox1.Controls.Add(this.lbl8);
+            this.groupBox1.Controls.Add(this.lbl7);
+            this.groupBox1.Controls.Add(this.lbl6);
+            this.groupBox1.Controls.Add(this.lbl5);
+            this.groupBox1.Controls.Add(this.lbl4);
+            this.groupBox1.Controls.Add(this.lbl3);
+            this.groupBox1.Controls.Add(this.ldl2);
+            this.groupBox1.Controls.Add(this.lbl1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 306);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please Enter your Information";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(90, 75);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 12;
+            this.txtEmail.Text = "scott&neit.edu";
+            // 
+            // dtpdate
+            // 
+            this.dtpdate.Location = new System.Drawing.Point(49, 146);
+            this.dtpdate.Name = "dtpdate";
+            this.dtpdate.Size = new System.Drawing.Size(200, 20);
+            this.dtpdate.TabIndex = 11;
+            // 
+            // cmbState
+            // 
+            this.cmbState.FormattingEnabled = true;
+            this.cmbState.Location = new System.Drawing.Point(78, 101);
+            this.cmbState.Name = "cmbState";
+            this.cmbState.Size = new System.Drawing.Size(121, 21);
+            this.cmbState.TabIndex = 10;
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Location = new System.Drawing.Point(7, 260);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(41, 13);
+            this.lbl10.TabIndex = 9;
+            this.lbl10.Text = "Work:";
+            // 
+            // lbl9
+            // 
+            this.lbl9.AutoSize = true;
+            this.lbl9.Location = new System.Drawing.Point(7, 231);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(32, 13);
+            this.lbl9.TabIndex = 8;
+            this.lbl9.Text = "Cell:";
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.Location = new System.Drawing.Point(7, 202);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(43, 13);
+            this.lbl8.TabIndex = 7;
+            this.lbl8.Text = "Home:";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lbl7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.Location = new System.Drawing.Point(7, 171);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(135, 13);
+            this.lbl7.TabIndex = 6;
+            this.lbl7.Text = "List of Phone Numbers";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(7, 146);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(36, 13);
+            this.lbl6.TabIndex = 5;
+            this.lbl6.Text = "date:";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(7, 124);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(29, 13);
+            this.lbl5.TabIndex = 4;
+            this.lbl5.Text = "Zip:";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(7, 101);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(41, 13);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "State:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(6, 78);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(41, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Email:";
+            // 
+            // ldl2
+            // 
+            this.ldl2.AutoSize = true;
+            this.ldl2.Location = new System.Drawing.Point(7, 56);
+            this.ldl2.Name = "ldl2";
+            this.ldl2.Size = new System.Drawing.Size(56, 13);
+            this.ldl2.TabIndex = 1;
+            this.ldl2.Text = "Address:";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(7, 32);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(43, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(297, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(288, 306);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Employees Information";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(32, 32);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(232, 232);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSubmit.Location = new System.Drawing.Point(22, 371);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(563, 36);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(90, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 13;
+            this.txtName.Text = "Scott";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(90, 53);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 14;
+            this.txtAddress.Text = "441 Broadway";
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(78, 124);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(64, 20);
+            this.txtZip.TabIndex = 15;
+            this.txtZip.Text = "02840";
+            // 
+            // txtHome
+            // 
+            this.txtHome.Location = new System.Drawing.Point(66, 202);
+            this.txtHome.Name = "txtHome";
+            this.txtHome.Size = new System.Drawing.Size(100, 20);
+            this.txtHome.TabIndex = 16;
+            this.txtHome.Text = "401-218-8796";
+            // 
+            // txtCell
+            // 
+            this.txtCell.Location = new System.Drawing.Point(66, 231);
+            this.txtCell.Name = "txtCell";
+            this.txtCell.Size = new System.Drawing.Size(100, 20);
+            this.txtCell.TabIndex = 17;
+            this.txtCell.Text = "401-218-8796";
+            // 
+            // txtWork
+            // 
+            this.txtWork.Location = new System.Drawing.Point(66, 257);
+            this.txtWork.Name = "txtWork";
+            this.txtWork.Size = new System.Drawing.Size(100, 20);
+            this.txtWork.TabIndex = 18;
+            this.txtWork.Text = "401-218-8796";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 438);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Personel File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label ldl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DateTimePicker dtpdate;
+        private System.Windows.Forms.ComboBox cmbState;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtWork;
+        private System.Windows.Forms.TextBox txtCell;
+        private System.Windows.Forms.TextBox txtHome;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Quest 4/Quest 4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quest_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //adding information in combo "status" box
+            //All states i have lived in
+            cmbState.Items.Add("Michigan");
+            cmbState.Items.Add("South Carolina");
+            cmbState.Items.Add("Florida");
+            cmbState.Items.Add("California");
+            cmbState.Items.Add("Rhode Island");
+            cmbState.Items.Add("Georgia");
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            //String to hold all output
+            //string result = "";
+
+            //Find where the "@" is:
+            //int intAtLocation = txtEmail.Text.IndexOf("@", 0);
+
+            //Find the position of the last character (unknown)
+            //int intLastPosition = txtEmail.Text.Length - 1;
+
+            //(1st way)Get the domain portion of the email address
+            //string strDomain = txtEmail.Text.Substring(intAtLocation+1);
+
+            //(2nd way)Get the domain portion of the email adress
+            //substring(a start index, a length)
+            //string strDomain = txtEmail.Text.Substring(intAtLocation + 1, ((intLastPosition) - intAtLocation));
+
+            //Get the person's email without domain (spacific user)
+            //string email = txtEmail.Text;
+            //string strPerson = email.Substring(0, intAtLocation);
+
+            //Calculate output
+            //result += "\nDomain: " + strDomain;
+            //result += "\nPerson: " + strPerson;
+
+            //display output
+            //txtOutput.Text = result;
+            //Output of information
+            
+            
+            txtOutput.Text = dtpdate.Value.ToString();
+
+            
+
+            txtOutput.Text = txtOutput.Text + txtName + "," + cmbState.GetItemText(cmbState.SelectedItem);
+
+            //Censor output
+            //txtNotes.Text = Censor(txtNotes.Text);
+
+        }
+    }
+}
